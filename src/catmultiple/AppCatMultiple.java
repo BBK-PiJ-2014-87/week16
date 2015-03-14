8931041046209965d150dbeb2f78ec8fa511ad21
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-14 15:28:49 +0000

    Updated main method.

--- src/catmultiple/AppCatMultiple.java
+++ src/catmultiple/AppCatMultiple.java
@@ -3,20 +3,30 @@ package catmultiple;
 import cat.MyFileReader;
 
 /**
- * @author Workstation
+ * Application for reading multiple files.
+ * @author Vladimirs Ivanovs
  *
  */
 public class AppCatMultiple {
 	
+	/**
+	 * Reads multiple files.
+	 * @param fileNames
+	 */
 	public static void readMultiple(String[] fileNames){
 		MyFileReader reader = new MyFileReader();
 		for(String file: fileNames){
 			reader.readRes(file);
+			System.out.println("STATUS: COMPLETED READING FILE "+ file);
 		}
-		
 	}
+	
 	public static void main(String[] args) {
-		readMultiple(args);
+		try{
+			String[] args2 = {"test_read.txt","test_read2.txt","test_read4.txt","test_read3.txt"};
+			readMultiple(args2);
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("No file entered. Please enter a valid file name.");
+		}
 	}
-
 }
