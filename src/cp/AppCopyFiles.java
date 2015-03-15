9686577f71e86475bc84a0dc3097bdcd57bea1ea
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-15 15:25:09 +0000

    Added multipleCopy method. Fixed bugs. Stable working version.

--- src/cp/AppCopyFiles.java
+++ src/cp/AppCopyFiles.java
@@ -1,13 +1,18 @@
 package cp;
 
-import java.io.File;
-
 public class AppCopyFiles {
 	public static void main(String[] args) {
-		File file = new File("");
-		System.out.println(file.exists());
+		FileCopier cp = new FileCopier();
+		String[] names = {
+				 "test_read.txt",
+				 "test_read2.txt",
+				 "test_read3.txt",
+				 "test_read4.txt",
+				 "/Users/Workstation/Desktop/test"};
+		
+//		String[] names = { "test_read2.txt", "test_read4.txt" };
+		
+		//RUN APPLICATION
+		cp.chooseCopyMethod(names);
 	}
-	
-	
-
 }
--- src/cp/FileCopier.java
+++ src/cp/FileCopier.java
@@ -1,11 +1,40 @@
 package cp;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.Scanner;
 
 public class FileCopier {
+	
+	/**
+	 * Decides which copy method to use based on the input.
+	 * @param names at least first one is a file name and the last one is a directory name
+	 */
+	public void chooseCopyMethod(String[] names){
+		File check = new File(names[names.length-1]);
+		if(names.length ==2){
+			System.out.println("TWO FILES METHOD");
+			copyOne(names[0], names[1]);
+		}else if (names.length>2 && check.isDirectory()){
+			System.out.println("MULTIPLE FILES METHOD");
+			copyMultiple(names);
+		}else{
+			System.out.println("ERROR: Invalid entry.");
+		}
+	}
+	
+	/**
+	 * Copies multiple files to a given directory.
+	 * @param names
+	 */
+	private void copyMultiple(String names[]){
+		for(int i =0; i< names.length-1; i++){
+			copyOne(names[i], names[names.length-1]+File.separator+names[i]);
+		}
+	}
 
 	/**
 	 * Copies file to specified file.
@@ -13,25 +42,28 @@ public class FileCopier {
 	 * @param from source file name/path as String
 	 * @param to target file name/path as String
 	 */
-	public void copyFiles(String from, String to) {
+	public void copyOne(String from, String to) {
 		File source = new File(from);
 		if (sourceExists(source)) {
 			File target = new File(to);
-			try {
-				if (targetExists(target) && confirmReplace()) {
+			try{
+				if(!targetExists(target)){
+					Files.copy(source.toPath(), target.toPath());
+					System.out.println("File " + target.getName()
+							+ " COPIED!");
+				} else if (target.exists() && confirmReplace()) {
 					Files.copy(source.toPath(), target.toPath(),
 							REPLACE_EXISTING);
-					System.out.println("File " + target.getName()
-							+ " replaced.");
-				} else {
-					Files.copy(source.toPath(), target.toPath());
-					System.out.println("File " + target.getName() + " copied");
+					System.out.println("File " + target.getName() + " REPLACED!");
+				}else{
+					System.out.println("File WASN'T copied!");
 				}
-			} catch (IOException e) {
+			}catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
+			
 
 	/**
 	 * Checks if source file exists.
@@ -41,10 +73,10 @@ public class FileCopier {
 	 */
 	private boolean sourceExists(File fileToCheck) {
 		if (fileToCheck.exists()) {
+			System.out.println("Source file "+fileToCheck.getName()+ " exists.");
 			return true;
 		} else {
-			System.out
-					.println("File to copy doesn't exist. Please enter another name.");
+			System.out.println("Source file "+fileToCheck.getName()+ " DOESN'T exist.");
 			return false;
 		}
 	}
@@ -56,11 +88,11 @@ public class FileCopier {
 	 * @return true if file exists, and false otherwise.
 	 */
 	private boolean targetExists(File fileToCheck) {
-		if (fileToCheck.exists()) {
-			return true;
-		} else {
-			System.out.println("File to copy doesn't exist.");
+		if (!fileToCheck.exists()) {
+			System.out.println("Target file "+fileToCheck.getName()+  " DOESN'T exist.");
 			return false;
+		} else {
+			return true;
 		}
 	}
 
@@ -92,11 +124,12 @@ public class FileCopier {
 		// if string is invalid it loops 3 times before exiting with the false
 		// return
 		do {
-			System.out
-					.println("Files with such name exists.Do you want to overwrite it? y/n");
-			answer = System.console().readLine();
+			System.out.println("Files already exists.Do you want to replace it? y/n");
+			Scanner in = new Scanner(System.in);
+			answer = in.nextLine();
 			attempts--;
-		} while (isValidEntry(answer) && attempts >= 3);
+		} while (isValidEntry(answer) && attempts > 0);
+		
 		if (answer.charAt(0) == 'y')
 			return true;
 		else
