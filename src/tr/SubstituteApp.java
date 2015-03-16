56638a5e1d3eafcf119e8541a6b78b4643cefc01
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-16 01:38:56 +0000

    Added comments

--- src/tr/SubstituteApp.java
+++ src/tr/SubstituteApp.java
@@ -1,5 +1,10 @@
 package tr;
 
+/**
+ * Application for changing text in files.
+ * @author Vladimirs Ivanovs
+ *
+ */
 public class SubstituteApp {
 	public static void main(String[] args) {
 		Tr app;
--- src/tr/Tr.java
+++ src/tr/Tr.java
@@ -8,6 +8,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Application for changing each occurrence of a string with 
+ * another defined string.
+ * 
+ * @author Vladimirs Ivanovs
+ *
+ */
 public class Tr {
 	private File source; 			
 	private File target;
@@ -15,6 +22,13 @@ public class Tr {
 	private String newText; 	//replacement text
 	public String fileContent;	//all content of the file
 	
+	/**
+	 * This constructor is used to save changes to new file.
+	 * @param source
+	 * @param target
+	 * @param oldText
+	 * @param newText
+	 */
 	public Tr(String source, String target, String oldText, String newText){
 		this.source = new File(source);
 		this.target = new File(target);
@@ -22,6 +36,12 @@ public class Tr {
 		this.newText = newText;
 	}
 	
+	/**
+	 * This constructor is used if we want to save changes in current file.
+	 * @param source 
+	 * @param oldText
+	 * @param newText
+	 */
 	public Tr(String source, String oldText, String newText){
 		this.source = new File(source);
 		target = this.source;
@@ -29,16 +49,25 @@ public class Tr {
 		this.newText = newText;
 	}
 	
+	/**
+	 * Reads the file, makes changes to the text and writes them to the file.
+	 */
 	public void run(){
 		readContent();
 		replaceText();
 		writeContent();
 	}
 	
+	/**
+	 * Replaces old string with new ones.
+	 */
 	private void replaceText(){
 		fileContent = fileContent.replaceAll(oldText, newText);
 	}
 	
+	/**
+	 * Reads content of a file and saves it as one string.
+	 */
 	private void readContent(){
 		StringBuilder content = new StringBuilder();
 		try(BufferedReader reader = new BufferedReader(new FileReader(source))){
@@ -57,6 +86,9 @@ public class Tr {
 		
 	}
 	
+	/**
+	 * Writes content to given file.
+	 */
 	private void writeContent(){
 		try(BufferedWriter writer = new BufferedWriter(new FileWriter(target))){
 			writer.write(fileContent);
